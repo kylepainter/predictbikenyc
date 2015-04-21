cd340ca768731aeb7ca0a99f1e7a5f6f4441a974
Kyle Painter <kylepainter@gmail.com>
2015-04-21 16:44:33 -0400

    what the?

--- run.py
+++ run.py
@@ -3,7 +3,9 @@
 #flaskfirst = "/sites/flaskfirst"
 #if not flaskfirst in sys.path:
 #    sys.path.insert(0, flaskfirst)
-
+import os
 
 from app import app
-app.run(host="0.0.0.0")
+#
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
