851be391c1e955403a0478bb23b0b2a289a2b8da
Kyle Painter <kylepainter@gmail.com>
2015-04-21 16:01:14 -0400

    run file addition

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 #!flask/bin/python
+import sys
+flaskfirst = "/sites/flaskfirst"
+if not flaskfirst in sys.path:
+    sys.path.insert(0, flaskfirst)
+
+
 from app import app
 app.run(host="0.0.0.0")
