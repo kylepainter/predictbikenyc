daa50feec19d32ad886459698dbdb8b540fc2e92
Kyle Painter <kylepainter@gmail.com>
2015-04-21 16:39:23 -0400

    what the?

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!flask/bin/python
-import sys
-flaskfirst = "/sites/flaskfirst"
-if not flaskfirst in sys.path:
-    sys.path.insert(0, flaskfirst)
+#import sys
+#flaskfirst = "/sites/flaskfirst"
+#if not flaskfirst in sys.path:
+#    sys.path.insert(0, flaskfirst)
 
 
 from app import app
