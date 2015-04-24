7522ac695a3704201af13fc7c994ed68fd610861
kylepainter <kylepainter@gmail.com>
2015-04-24 17:32:51 -0400

    run and requirements

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 #!flask/bin/python
-#import sys
-#flaskfirst = "/sites/flaskfirst"
-#if not flaskfirst in sys.path:
-#    sys.path.insert(0, flaskfirst)
 import os
-
 from app import app
-#
+
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
